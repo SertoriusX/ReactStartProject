b14116b3a92aea2bb4d46f6aa2655a68bb898075
Tsvet <youremail@example.com>
2026-04-29 18:57:59 +0200

    feat: allow setting base url via npm run env

--- scripts/init-env.js
+++ scripts/init-env.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import path from 'path';
 
 const envPath = path.resolve('.env');
+const url = process.argv[2] || '';
 
 if (fs.existsSync(envPath)) {
   console.log('.env file already exists');
 } else {
-  fs.writeFileSync(envPath, '');
-  console.log('.env file created');
+  fs.writeFileSync(envPath, `VITE_BASE_URL='${url}'\n`);
+  console.log(`.env file created: VITE_BASE_URL='${url}'`);
 }
